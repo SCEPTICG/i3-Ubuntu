ee885ee37cfabd290c6c2807c4a347967f7aa139
SCEPTICG <114002154+SCEPTICG@users.noreply.github.com>
2022-11-06 01:58:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-de
 libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev \
 libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev \ 
 libxkbcommon-x11-dev autoconf xutils-dev libtool automake nodejs npm meson asciidoc \
-libxcb-shape0-dev
+libxcb-shape0-dev libxcb-xrm-dev -y
 
 wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.11.tar.gz
 tar -xvzf libiconv-1.11.tar.gz
