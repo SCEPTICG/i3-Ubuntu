2ae8bf724b52aee886b2d10041bb085bb63b481a
SCEPTICG <114002154+SCEPTICG@users.noreply.github.com>
2022-11-06 02:04:15 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #Instalamos las dependencias
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
 libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev \
-libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev \ 
+libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev \
 libxkbcommon-x11-dev autoconf xutils-dev libtool automake nodejs npm meson asciidoc \
 libxcb-shape0-dev libxcb-xrm-dev -y
 
